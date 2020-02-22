1af4c57ce19b81ed9ed95af8fe9a155a77328cea
Anette <an.weyers@web.de>
2020-02-22 15:08:17 +0100

    feat: webpack configured, incl. plugins, loaders, modes

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+      dotenv.config();
+var aylien = require("aylien_textapi");
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -13,10 +16,28 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+// set aylien API credentials
+// NOTICE that textapi is the name I used, but it is arbitrary.
+// You could call it aylienapi, nlp, or anything else, 
+//   just make sure to make that change universally!
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+textapi.sentiment({
+    'text': 'John is a very good football player!'
+  }, function(error, response) {
+    if (error === null) {
+      console.log(response);
+    }
+  });
+     
